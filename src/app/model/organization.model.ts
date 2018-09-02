cad784e10424a2ed34f95cad0e405ad7825f8c0e
sswitlik <szczepan.switlik@gmail.com>
2018-09-02 18:21:49 +0200

    tests for many-to-many + using Set collection

--- src/app/model/organization.model.ts
+++ src/app/model/organization.model.ts
@@ -1,27 +1,26 @@
 import { Person } from './person.model';
 import { OneToMany } from '../standalone/relation/one-to-many.standalone';
-import { removeFromArray } from '../function/remove-from-array';
 
 export class Organization {
 
   name: string;
 
-  workers: Person[];
+  workers: Set<Person>;
 
   private workersRelation = new OneToMany<Organization, Person>(this, 'organizationRelation', {
     add: relative => {
-      this.workers.push(relative);
+      this.workers.add(relative);
       console.log(`${this.name} => ${relative.name} add`);
     },
     erase: relative => {
-      removeFromArray(this.workers, relative);
+      this.workers.delete(relative);
       console.log(`${this.name} => ${relative.name} erase`);
     }
   });
 
   constructor(name: string) {
     this.name = name;
-    this.workers = [];
+    this.workers = new Set<Person>();
   }
 
   addWorker(worker: Person) {
